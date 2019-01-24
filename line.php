fb5bba4a4fb5f5193984baa0f65416270edcf189
pitchsirinjiffy <46991078+pitchsirinjiffy@users.noreply.github.com>
2019-01-24 15:57:49 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'GRWBwhmVWihDmi61pv+QkJNHi9qrNIk4NMgIQVVcjaHaytUtu+RDHNrTRxET8caBM6muW1g+0c3J0hz3C1v4gDeIvMzSLHgyCCbisurv515cbyg84FhnT4JsvNvAe3bdg4LHgpc7FMd5K/MgGRsMHAdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
