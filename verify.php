6cb3124f3517976c644143ded948e712a6d64b34
pitchsirinjiffy <46991078+pitchsirinjiffy@users.noreply.github.com>
2019-01-24 15:57:06 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,6 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'GRWBwhmVWihDmi61pv+QkJNHi9qrNIk4NMgIQVVcjaHaytUtu+RDHNrTRxET8caBM6muW1g+0c3J0hz3C1v4gDeIvMzSLHgyCCbisurv515cbyg84FhnT4JsvNvAe3bdg4LHgpc7FMd5K/MgGRsMHAdB04t89/1O/w1cDnyilFU=
+';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
